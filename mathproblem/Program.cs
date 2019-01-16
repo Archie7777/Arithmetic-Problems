61f915bdb69d69eeb15bf1a52cf30bd504f494ef
Yixuan Guo <2761683494@qq.com>
2019-01-16 16:43:03 +0800

    change the old Problem type variable into the new Expression type

--- mathproblem/Program.cs
+++ mathproblem/Program.cs
@@ -10,45 +10,46 @@ namespace mathproblem
     {
         static void Main(string[] args)
         {
-            // Test if input arguments were supplied:
-            if (args.Length == 0)
+            ArgumentParser argument = new ArgumentParser(args);
+            if (argument.GetLength() == 0)
             {
                 System.Console.WriteLine("Please enter an argument.");
                 System.Console.WriteLine("Usage:\n" +
                     "generate 1000 problems: -g absolute_path_of_output_file\n" +
                     "play games: -p num_of_problem_you_want_to_play\n" +
                     "solve problems: -s absolute_path_of_problem_file");
-                return ;
-            }
-
-            if (args[0] == "-g")
-            {
-                //====================================
-                for(int i = 0; i < 10; i++)
-                {
-                    Problem p = new Problem();
-                    while (p.IsInvalid() == 1)
-                    {
-                        p = new Problem();
-                    }
-                    p.PrintProblem();
-                    p.PrintAnswer();
-                }
-
-                //====================================
-                //Console.WriteLine("generate problem");
                 return;
             }
 
-            if (args[0] == "-p")
+            if (argument.Get(1) == "-g")
+            {
+                if (argument.Has("-d") && argument.Has("-c"))
+                    Console.WriteLine("Please input correct argument");
+                else if (argument.Has("-d"))
+                {
+                    Expression.SetPrintType(Expression.PowerOpPrintType.doubleStar);
+                    Expression.SetPowerOp(Expression.IsPowerOp.yes);
+                }
+                else if (argument.Has("-c"))
+                {
+                    Expression.SetPrintType(Expression.PowerOpPrintType.caret);
+                    Expression.SetPowerOp(Expression.IsPowerOp.yes);
+                }
+                else
+                    Expression.SetPowerOp(Expression.IsPowerOp.no);
+                if (argument.Has("-h"))
+                    Expression.SetDifficulty(Expression.Difficulty.hard);
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"./Expressions.txt", false))
+                {
+                    for (int i = 0; i < argument.ToInt(2); i++)
+                        Generate(file);
+                }
+                return;
+            }
+
+            if (argument.Get(1) == "-p")
             {
                 Console.WriteLine("play");
-                return;
-            }
-
-            if (args[0] == "-s")
-            {
-                Console.WriteLine("solve problem");
                 return;
             }
 
@@ -58,6 +59,15 @@ namespace mathproblem
                 "play games: -p num_of_problem_you_want_to_play\n" +
                 "solve problems: -s absolute_path_of_problem_file");
             return;
+        }
+        static void Generate(System.IO.StreamWriter file)
+        {
+            Expression ep = new Expression();
+            while (ep.IsInvalid())
+            {
+                ep = new Expression();
+            }
+            ep.PrintExpressionWithAnswerToFile(file);
         }
     }
 }
